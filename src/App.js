4962bb4f14218ccd26e866bb462e3ced11e7944f
Dhareppa Halli <dharepps@gmail.com>
2024-07-01 15:33:57 +0530

    Changes For Added Dymanic Routes in E-Learning App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import Hero from "./pages/app/hero/Hero";
 import Nav from "./components/nav/Nav";
 import Courses from "./pages/app/courses/Courses";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Details from "./pages/app/details/Details";
+import Learn from "./pages/app/learn/Learn";
+
 function App() {
+  const browserRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Nav />,
+      children: [
+        { path: "", element: <Hero /> },
+        {
+          path: "/courses",
+          children: [
+            { index: true, element: <Courses /> },
+            { path: ":courseId", element: <Details /> },
+          ],
+        },
+        { path: "/learn/:courseId", element: <Learn /> },
+      ],
+    },
+  ]);
+
   return (
-    <div className="App">
-      <Nav />
-      <Hero />
-      <Courses />
-    </div>
+    <>
+      <RouterProvider router={browserRouter} />
+    </>
   );
 }
 
--- src/pages/app/chapter/Chapter.jsx
+++ src/pages/app/chapter/Chapter.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import styles from "./Chapter.module.css";
 
 function Chapter() {
   return (
     <div>
-      <h1>**CHAPTER TITLE**</h1>
-      <h2>**CHAPTER DESCRIPTION**</h2>
-      <p className={styles.para}>**CHAPTER**</p>
+      <h1>{chapter.title}</h1>
+      <p>{chapter.description}</p>
+      <p className={styles.para}>{chapter.details}</p>
       <br />
       <br />
       <div className={styles.videos}>
-        {/*TASK:5 CHAPTER VIDEO SRC SHOULD BE GIVEN IN THE SOURCE */}
         <iframe
           width="800"
           height="560"
-          src=""
-          title="React Video"
+          src={chapter.video}
+          title="YouTube video player"
           frameborder="1"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowfullscreen
